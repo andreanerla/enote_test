15be5a5f5762cf76be8978d53b9a7966b294bad5
andreanerla <54369118+andreanerla@users.noreply.github.com>
2020-07-19 17:09:05 +0200

    Delete enote_person_bulk.sql

--- enote_person_bulk.sql
+++
@@ -1,9 +0,0 @@
-BULK INSERT enote_person
-  FROM 'C:\Users\andre\Desktop\Downloads\Data Analyst assignment_enote\BI_assignment_person.csv' 
-  WITH 
-  ( 
-        FIRSTROW = 2, 
-        MAXERRORS = 0, 
-        FIELDTERMINATOR = ',', 
-        ROWTERMINATOR = '\n' 
-   )
