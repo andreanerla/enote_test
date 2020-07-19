913c00a5dcbe20a5810799306030a32298888b27
andreanerla <54369118+andreanerla@users.noreply.github.com>
2020-07-19 17:08:55 +0200

    Delete enote_account_bulk.sql

--- enote_account_bulk.sql
+++
@@ -1,9 +0,0 @@
-BULK INSERT enote_account
-  FROM 'C:\Users\andre\Desktop\Downloads\Data Analyst assignment_enote\BI_assignment_account.csv' 
-  WITH 
-  ( 
-        FIRSTROW = 2, 
-        MAXERRORS = 0, 
-        FIELDTERMINATOR = ',', 
-        ROWTERMINATOR = '\n' 
-   )
