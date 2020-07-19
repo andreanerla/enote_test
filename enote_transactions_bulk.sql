1a79da9191367a51c3e95279e18a8d19eb760009
andreanerla <54369118+andreanerla@users.noreply.github.com>
2020-07-19 17:09:23 +0200

    Delete enote_transactions_bulk.sql

--- enote_transactions_bulk.sql
+++
@@ -1,9 +0,0 @@
-BULK INSERT enote_transactions
-  FROM 'C:\Users\andre\Desktop\Downloads\Data Analyst assignment_enote\BI_assignment_transaction.csv' 
-  WITH 
-  ( 
-        FIRSTROW = 2, 
-        MAXERRORS = 0, 
-        FIELDTERMINATOR = ',', 
-        ROWTERMINATOR = '\n' 
-   )
